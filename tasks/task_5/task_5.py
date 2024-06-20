b4ef44fa72c87fbf770b6478215e3d0e2dbd69f6
marcus0329 <skuan@dons.usfca.edu>
2024-06-20 16:27:26 -0700

    complete task 5

--- tasks/task_5/task_5.py
+++ tasks/task_5/task_5.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import chromadb
 import streamlit as st
 sys.path.append(os.path.abspath('../../'))
 from tasks.task_3.task_3 import DocumentProcessor
@@ -57,6 +58,15 @@ class ChromaCollectionCreator:
         # Use a TextSplitter from Langchain to split the documents into smaller text chunks
         # https://python.langchain.com/docs/modules/data_connection/document_transformers/character_text_splitter
         # [Your code here for splitting documents]
+        text_splitter = CharacterTextSplitter(
+            separator="\n\n",
+            chunk_size=1000,
+            chunk_overlap=200,
+            length_function=len,
+            is_separator_regex=False,
+        )
+
+        texts = text_splitter.split_documents(self.processor.pages)
         
         if texts is not None:
             st.success(f"Successfully split pages to {len(texts)} documents!", icon="✅")
@@ -65,6 +75,9 @@ class ChromaCollectionCreator:
         # https://docs.trychroma.com/
         # Create a Chroma in-memory client using the text chunks and the embeddings model
         # [Your code here for creating Chroma collection]
+        chroma_client = chromadb.Client()
+        self.db = Chroma.from_documents(texts, self.embed_model, client=chroma_client, collection_name="my_collection")
+
         
         if self.db:
             st.success("Successfully created Chroma Collection!", icon="✅")
@@ -93,7 +106,7 @@ if __name__ == "__main__":
     
     embed_config = {
         "model_name": "textembedding-gecko@003",
-        "project": "YOUR PROJECT ID HERE",
+        "project": "gemini-quizify-426423",
         "location": "us-central1"
     }
     
