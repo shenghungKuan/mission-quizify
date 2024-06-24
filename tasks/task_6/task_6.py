971fd4f7406fc4b6b0050f733c0662ace85b8f3b
marcus0329 <skuan@dons.usfca.edu>
2024-06-24 14:29:28 -0700

    task_6 complete

--- tasks/task_6/task_6.py
+++ tasks/task_6/task_6.py
@@ -45,7 +45,7 @@ if __name__ == "__main__":
     # Configuration for EmbeddingClient
     embed_config = {
         "model_name": "textembedding-gecko@003",
-        "project": "YOUR PROJECT ID HERE",
+        "project": "gemini-quizify-426423",
         "location": "us-central1"
     }
     
@@ -57,6 +57,10 @@ if __name__ == "__main__":
         # 2) Initalize the EmbeddingClient from Task 4 with embed config
         # 3) Initialize the ChromaCollectionCreator from Task 5
         ####### YOUR CODE HERE #######
+        processor = DocumentProcessor()
+        processor.ingest_documents()
+        embed_client = EmbeddingClient(**embed_config)
+        chroma_creator = ChromaCollectionCreator(processor, embed_client)
 
         with st.form("Load Data to Chroma"):
             st.subheader("Quiz Builder")
@@ -66,6 +70,10 @@ if __name__ == "__main__":
             # 4) Use streamlit widgets to capture the user's input
             # 4) for the quiz topic and the desired number of questions
             ####### YOUR CODE HERE #######
+            topic_input = st.text_input("Enter the topic for the quiz:")
+    
+            number_of_questions = st.slider("Select the number of questions:", min_value=1, max_value=10, value=5, step=1)
+            
             
             document = None
             
@@ -74,9 +82,10 @@ if __name__ == "__main__":
                 ####### YOUR CODE HERE #######
                 # 5) Use the create_chroma_collection() method to create a Chroma collection from the processed documents
                 ####### YOUR CODE HERE #######
+                chroma_creator.create_chroma_collection()
                     
                 # Uncomment the following lines to test the query_chroma_collection() method
-                # document = chroma_creator.query_chroma_collection(topic_input) 
+                document = chroma_creator.query_chroma_collection(topic_input) 
                 
     if document:
         screen.empty() # Screen 2
